1e7d5f7fc4219a687b7e6a922b8bbad175d98178
Lixquid <lixquid@lixquid.com>
2021-12-21 03:04:30 +0000

    Update tags and formatting for LPArchive Dark Mode

--- lparchive-darkmode.js
+++ lparchive-darkmode.js
@@ -2,22 +2,22 @@
 // @name         LP Archive Dark Mode
 // @namespace    https://lixquid.com/
 // @version      0.1
-// @description  Easy on the eyes.
-// @icon         https://lparchive.org/favicon.ico
 // @author       Lixquid
+// @description  Forces a dark mode for the LP Archive.
+// @supportURL   https://github.com/Lixquid/js-monkey-userscripts/issues
+// @icon         https://lparchive.org/favicon.ico
 // @match        https://lparchive.org/*
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+    "use strict";
 
-    document.getElementsByTagName("html")[0].style.background = "#222"
-    document.body.style.background = "#222"
-    document.body.style.color = "#eee"
-    var content = document.getElementById("content")
+    document.getElementsByTagName("html")[0].style.background = "#222";
+    document.body.style.background = "#222";
+    document.body.style.color = "#eee";
+    var content = document.getElementById("content");
     if (content) {
-        content.style.background = "#333"
-        content.style.color = "#eee"
+        content.style.background = "#333";
+        content.style.color = "#eee";
     }
-    
 })();
