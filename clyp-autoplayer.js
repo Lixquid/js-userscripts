525b10337745732f939815b5869d0686ad782154
Lixquid <lixquid@lixquid.com>
2023-06-07 16:33:09 +0100

    Update Clyp Autoplayer script to use GM_setValue and GM_getValue instead of localStorage

--- clyp-autoplayer.js
+++ clyp-autoplayer.js
@@ -1,23 +1,24 @@
 // ==UserScript==
 // @name         Clyp Autoplayer
 // @namespace    https://lixquid.com/
-// @version      0.1
+// @version      0.2
 // @author       Lixquid
 // @description  Automatically plays Clyp audio files as they load, and ensures only one Clyp window is kept open at a time.
 // @supportURL   https://github.com/Lixquid/js-monkey-userscripts/issues
 // @icon         https://www.google.com/s2/favicons?domain=clyp.it
 // @match        https://clyp.it/*
-// @grant        localStorage
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
 
 (function () {
     "use strict";
 
     const id = String(Math.random());
-    localStorage.setItem("AUTOPLAY_ID", id);
+    GM.setValue("AUTOPLAY_ID", id);
 
     setInterval(() => {
-        if (localStorage.getItem("AUTOPLAY_ID") !== id) {
+        if (GM.getValue("AUTOPLAY_ID") !== id) {
             close();
         }
         const e = document.querySelector("div.play-button:nth-child(2)");
